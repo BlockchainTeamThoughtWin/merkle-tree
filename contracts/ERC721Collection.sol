abd4a43ee42fa7e6059592ab45cccd975208de5e
chandu-thoughtwin <chandrashekhar.thoughtwin@gmail.com>
2022-09-30 16:48:42 +0530

    first commit

---
+++ contracts/ERC721Collection.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Collection is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    bytes32 public merkleRoot;
+
+    constructor() ERC721("MY-NFT Collection", "MYNFT") {}
+
+    function setMerkleRoot(bytes32 _root) public onlyOwner {
+        merkleRoot = _root;
+    }
+
+    function mint(string memory tokenURI, bytes32[] calldata proof)
+        public
+        returns (uint256)
+    {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(proof, merkleRoot, leaf),
+            "user is not verify"
+        );
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,63 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+
+describe("Collection", function () {
+
+  let whitelist = [];
+  let leafNodes;
+  let root;
+  let collection;
+  let proof;
+
+  before(async function () {
+
+    accounts = await ethers.getSigners();
+    whitelist.push(accounts[0].address)
+    whitelist.push(accounts[1].address)
+    whitelist.push(accounts[2].address)
+    whitelist.push(accounts[3].address)
+    leafNodes = whitelist.map((node) => keccak256(node));
+
+    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
+    root = merkleTree.getHexRoot().toString();
+    proof = merkleTree.getHexProof(leafNodes[2])
+
+    const Collection = await ethers.getContractFactory("Collection");
+    collection = await Collection.deploy();
+    await collection.deployed();
+  });
+
+  describe("Collection minting test ", async function () {
+
+    it("Should set root hash", async function () {
+
+      const tx = await collection.connect(accounts[0]).setMerkleRoot(root);
+      await tx.wait();
+
+      expect(await collection.merkleRoot()).to.equal(root);
+
+    });
+
+    it("Should mint NFTs via whitelsit ", async function () {
+      const tx = await collection.connect(accounts[2]).mint("hello world", proof);
+      expect(await collection.ownerOf(1)).to.equal(accounts[2].address);
+
+    });
+
+    it("Should not mint NFTs via  whitelsit with non vaild proofs ", async function () {
+      await expect(collection.connect(accounts[1]).mint("hello world", proof)).to.be.revertedWith('user is not verify');
+    });
+
+    it("Should not mint NFTs via  non whitelsit  ", async function () {
+
+      await expect(collection.connect(accounts[5]).mint("hello world", proof)).to.be.revertedWith('user is not verify');
+
+    });
+
+
+  });
+
+});
